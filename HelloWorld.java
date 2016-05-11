a484ef84de3a1d551966d36a52238fa5791c8137
Chris Simmons <Chris Simmons>
2016-05-11 15:23:27 -0600

    Added testing...

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 class hello{
 	public static void main(String[] args){
-		System.out.println("Hello GitHub!");
+		System.out.println("Hello GitHub world!");
+		System.out.println("Testing...");
 	}
 }
